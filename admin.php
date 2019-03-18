ed9e24065bba7e099cc54be18ead0b259e7791b8
Knux2 <ben_ashfield@hotmail.com>
2019-03-18 10:36:52 +0000

    Added basic CSS and begin to add dropdown button on form page

--- admin.php
+++ admin.php
@@ -13,20 +13,26 @@
 </head>
 
 <body>
-    <h1>Admin Page</h1>
-    <h2>Welcome Ben</h2>
-    <h3>About Me</h3>
-    Add additional information<form method="post">
-        <textarea rows="4" cols="100" name="add"></textarea>
-        <input type="submit" value="Add">
-    </form>
-    Edit information<form method="post">
-        <textarea rows="4" cols="100" name="edit"></textarea>
-        <input type="submit" value="Edit">
-    </form>
-    Delete information<form method="post">
-        <textarea rows="4" cols="100" name="delete"></textarea>
-        <input type="submit" value="Delete">
-    </form>
+    <header>
+        <h1>Admin Page</h1>
+        <h2>Welcome Ben</h2>
+        <h3>About Me</h3>
+    </header>
+    <div class="forms">
+        <p>Add additional information</p><form method="post">
+            <textarea rows="4" cols="100" name="add"></textarea>
+            <input type="submit" value="Add">
+        </form>
+        <p>Edit information</p>
+        <button onclick="" class="dropbtn">Select Paragraph to Edit</button>
+        <form method="post">
+            <textarea rows="4" cols="100" name="edit"></textarea>
+            <input type="submit" value="Edit">
+        </form>
+        <p>Delete information</p><form method="post">
+            <textarea rows="4" cols="100" name="delete"></textarea>
+            <input type="submit" value="Delete">
+        </form>
+    </div>
 </body>
 
