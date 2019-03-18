bd5d9b6c3c363bdf6f0db8a515e6d80845b386d3
Knux2 <ben_ashfield@hotmail.com>
2019-03-18 09:29:52 +0000

    Added original information from portfolio page to database

--- db/portfolio_db.sql
+++ db/portfolio_db.sql
@@ -7,7 +7,7 @@
 #
 # Host: 127.0.0.1 (MySQL 5.6.43)
 # Database: Portfolio
-# Generation Time: 2019-03-15 16:24:46 +0000
+# Generation Time: 2019-03-18 09:27:58 +0000
 # ************************************************************
 
 
@@ -20,18 +20,28 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 
-# Dump of table About_Me
+# Dump of table about_me
 # ------------------------------------------------------------
 
-DROP TABLE IF EXISTS `About_Me`;
+DROP TABLE IF EXISTS `about_me`;
 
-CREATE TABLE `About_Me` (
+CREATE TABLE `about_me` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `paragraph` varchar(500) NOT NULL DEFAULT '',
-  `deleted` tinyint(1) unsigned NOT NULL,
+  `deleted` tinyint(1) unsigned NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+LOCK TABLES `about_me` WRITE;
+/*!40000 ALTER TABLE `about_me` DISABLE KEYS */;
+
+INSERT INTO `about_me` (`id`, `paragraph`, `deleted`)
+VALUES
+	(1,'After working as a manager in the retail industry for 15 years I decided to change careers and become a web developer.\n            I am currently studying full stack development at the Mayden Academy over a 16 week course to learn about the skills and tools needed.',0),
+	(2,'In my spare time I enjoy online gaming. I particularly enjoy the competitive environment, trying to outfox and outplay opposing players\n            as well as playing together with a team strategising and working towards accomplishing a goal.',0);
+
+/*!40000 ALTER TABLE `about_me` ENABLE KEYS */;
+UNLOCK TABLES;
 
 
 
