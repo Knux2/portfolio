7a528cb64d2d18863d74cc52a4b60667888c03db
Knux2 <ben_ashfield@hotmail.com>
2019-03-19 15:16:36 +0000

    Added docblock to function

--- php/addMeFunctions.php
+++ php/addMeFunctions.php
@@ -1,5 +1,13 @@
 <?php
 
+/*
+ * Function adds new information to database
+ *
+ * @param $db PDO database connection
+ *
+ * @param $addParagraph string Information that will be added into database
+ */
+
 function addParagraph(PDO $db, string $addParagraph) {
     $query = $db->prepare("INSERT INTO `about_me` (`paragraph`) VALUES (:addParagraph);");
     $query->execute(['addParagraph'=>$addParagraph]);
