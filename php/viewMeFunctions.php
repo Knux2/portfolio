de61f8eb05fb7b34cbe3110749fc369cdb36d97c
Knux2 <ben_ashfield@hotmail.com>
2019-03-19 10:14:00 +0000

    Added Doc blocks and type hinting to viewMeFunctions

--- php/viewMeFunctions.php
+++ php/viewMeFunctions.php
@@ -1,12 +1,28 @@
 <?php
 
-function viewAboutMe(PDO $db) {
+/*
+ * Pulls array from database
+ *
+ * @param $db connection to database
+ *
+ * @return array Pulls multidimensional array from database
+ */
+
+function viewAboutMe(PDO $db) :array {
     $query = $db->prepare("SELECT `paragraph` FROM `about_me` WHERE `deleted` = '0';");
     $query->execute();
     return $query->fetchAll();
 }
 
-function getParagraph($viewAboutMe) {
+/*
+ * Breaks down multidimensional array into string
+ *
+ * @param $viewAboutMe Multidimensional array pulled from database
+ *
+ * @return string Paragraphs on database are displayed on portfolio page
+ */
+
+function getParagraph(array $viewAboutMe) :string {
     $result = '';
     foreach($viewAboutMe as $paragraph) {
         $result .= '<h2 class="font1">' . $paragraph['paragraph'] .'</h2>';
