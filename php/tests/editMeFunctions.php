1f0c76a196781305befae36b09552ecaf28d2130
Knux2 <ben_ashfield@hotmail.com>
2019-03-20 15:41:10 +0000

    Unit tests added for hidden Input function

--- php/tests/editMeFunctions.php
+++ php/tests/editMeFunctions.php
@@ -36,4 +36,34 @@ class StackTest extends Testcase
         $this->expectException(TypeError::class);
         getParagraph($input);
     }
+
+    public function hiddenInputSuccess()
+    {
+        $expected = '<option value=>This is a Paragraph</option>';
+        $input = [['paragraph'=>'This is a Paragraph']];
+        $case = hiddenInput($input);
+        $this->assertEquals($expected, $case);
+    }
+
+    public function hiddenInputFailure()
+    {
+        $expected = '<option value=></option>';
+        $input = [['paragraph'=>'']];
+        $case = hiddenInput($input);
+        $this->assertEquals($expected, $case);
+    }
+
+    public function hiddenInputMalformed()
+    {
+        $input = 101;
+        $this->expectException(TypeError::class);
+        hiddenInput($input);
+    }
+
+    public function hiddenInputMalformed2()
+    {
+        $input = 'This is a Paragraph';
+        $this->expectException(TypeError::class);
+        hiddenInput($input);
+    }
 }
