12340f0496d5f7dd6dd516058de0491358a3f43c
Knux2 <ben_ashfield@hotmail.com>
2019-03-21 13:34:28 +0000

    Added bind param and doc block to function

--- php/deleteMeFunctions.php
+++ php/deleteMeFunctions.php
@@ -1,6 +1,16 @@
 <?php
 
-function deleteAboutMe(PDO $db) :array {
+/*
+ * Changes selection from drop down to 1 in deleted column
+ *
+ * @param $db PDO Connection to database
+ *
+ * @param $deleteChoice string Links $deleteChoice to id field in database
+ *
+ * @return array Pulls multidimensional array from database
+ */
+
+function deleteAboutMe(PDO $db, string $deleteChoice) :array {
     $query = $db->prepare("UPDATE `about_me` SET `deleted` = 1 WHERE `id` = :deleteChoice;");
     $query->bindparam(':deleteChoice', $deleteChoice);
     $query->execute();
