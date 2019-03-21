26a4ce0a9ac12db5f700702311d5b2a9fd92f71f
Knux2 <ben_ashfield@hotmail.com>
2019-03-21 16:23:34 +0000

    Style sheet for login page added and loginPage.php modified

--- loginPage.php
+++ loginPage.php
@@ -9,17 +9,20 @@
 <head>
     <title>Bens Login Page</title>
     <link rel='stylesheet' type='text/css' href='css/normalise.css'>
-    <link rel='stylesheet' type='text/css' href='css/style.css'>
+    <link rel='stylesheet' type='text/css' href='css/loginPage.css'>
 </head>
 
 <body>
-    <h1>Login</h1>
-    <form method="POST" action="admin.php">
-        <h2>Username</h2>
-        <input id="username" type="text" name="username">
-        <h2>Password</h2>
-        <input id="password" type="password" name="password">
-        <input class="button" type="submit" name="login" value="Login">
-    </form>
+    <main>
+        <h1>Login</h1>
+        <form method="POST" action="admin.php">
+            <h2>Username</h2>
+            <input id="username" type="text" name="username">
+            <h2>Password</h2>
+            <input id="password" type="password" name="password">
+            <br>
+            <input class="button" type="submit" name="login" value="Login">
+        </form>
+    </main>
 </body>
 </html>
